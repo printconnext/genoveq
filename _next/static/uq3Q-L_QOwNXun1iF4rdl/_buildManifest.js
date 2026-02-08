5c0a415f0814ede03baae2186be24c46eb75e8b3
printconnext <printconnext@gmail.com>
2026-02-08 11:43:21 +0700

    Updates

--- _next/static/uq3Q-L_QOwNXun1iF4rdl/_buildManifest.js
+++ _next/static/uq3Q-L_QOwNXun1iF4rdl/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/genoveq//_next/:path+",
-        "destination": "/genoveq/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
